18db483e22efaf205cce0ffac2e9c9fd1d993503
DishitaShankar24 <90633779+DishitaShankar24@users.noreply.github.com>
2021-09-29 19:48:13 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,92 +1,115 @@
-var garden,rabbit;
-var gardenImg,rabbitImg;
-var apple,appleImg;
-var greenleaf, greenleafImg;
-var redleaf,redleafImg;
-var orangeleaf,orangeleafImg;
+var bow , arrow;
+var bowImage, arrowImage, green_balloonImage, red_balloonImage, pink_balloonImage ,blue_balloonImage, backgroundImage;
 
 function preload(){
-  gardenImg = loadImage("garden.png");
-  rabbitImg = loadImage("rabbit.png");
-  appleImg=loadImage("apple.png");
-  greenleafImg=loadImage("leaf.png")
-  redleafImg=loadImage("redImage.png");
-  orangeleafImg=loadImage("orangeLeaf.png");
-
-}
-
-function setup()
-{
   
-  createCanvas(550,450);
+  backgroundImage = loadImage("background0.png");
+  arrowImage = loadImage("arrow0.png");
+  bowImage = loadImage("bow0.png");
+  red_balloonImage = loadImage("red_balloon0.png");
+  green_balloonImage = loadImage("green_balloon0.png");
+  blue_balloonImage = loadImage("blue_balloon0.png");
+  pink_balloonImage = loadImage("pink_balloon0.png");
+}
+
+
+
+function setup() {
+  createCanvas(400, 400);
   
-// Background Sprite
-garden=createSprite(200,200);
-garden.addImage(gardenImg);
-garden.scale=1.4
-
-//creating green leaf
-greenleaf = createSprite(480,430,30,30);
-greenleaf.scale =0.07;
-greenleaf.addImage(greenleafImg);
-
-//creating red leaf
-redleaf = createSprite(180,400,30,30);
-redleaf.scale =0.065;
-redleaf.addImage(redleafImg);
-
-//creating rabbit
-rabbit = createSprite(180,340,30,30);
-rabbit.scale =0.12;
-rabbit.addImage(rabbitImg);
+  //creating background
+  scene = createSprite(0,0,400,400);
+  scene.addImage(backgroundImage);
+  scene.scale = 2.5
+  
+  // creating bow to shoot arrow
+  bow = createSprite(380,220,20,50);
+  bow.addImage(bowImage); 
+  bow.scale = 1;
+  
 
 }
 
+function draw() {
+ background(0);
+  // moving ground
+    scene.velocityX = -3 
 
+    if (scene.x < 0){
+      scene.x = scene.width/2;
+    }
+  
+  //moving bow
+  bow.y = World.mouseY
 
-function draw()
-{
-  background(0);
-  createapples();
-  createorangeleaf();
-  edges= createEdgeSprites();
-  rabbit.collide(edges);
-  //console.log(rabbit.x)
-   drawSprites();
-  rabbit.x=mouseX
+   // release arrow when space key is pressed
+  if (keyDown("space")) {
+    createArrow();
+  }
+  
+
+  //creating continous balloons
+  var select_balloon = Math.round(random(1,4));
+   if (World.frameCount % 100 === 0) {
+    if (select_balloon === 1) {
+      console.log(select_balloon);
+      redBalloon();
+    }
+    else if(select_balloon===2){
+greenBalloon();
+}
+else if(select_balloon===3){
+  blueBalloon();
+}
+else {
+  pinkBalloon();
+  }
+}
+  drawSprites();
+}
+// Creating  arrows for bow
+ function createArrow() {
+  var arrow= createSprite(300, 100, 60, 10);
+  arrow.addImage(arrowImage);
+  arrow.x = 360;
+  arrow.y=bow.y;
+  arrow.velocityX = -4;
+  arrow.lifetime = 100;
+  arrow.scale = 0.3;
 }
 
-//creating apple
-function createapples()
-{
 
-  if(frameCount%80===0)
- {
-   // write your code here 
-   apple = createSprite(50,100,30,30);
-   apple.scale =0.07;
-   apple.addImage(appleImg);
-   apple.velocityY=3;
-   apple.x= Math.round(random(50,450))
-   apple.life=60
- }
+function redBalloon() {
+  var red = createSprite(0,Math.round(random(20, 370)), 10, 10);
+  red.addImage(red_balloonImage);
+  red.velocityX = 3;
+  red.lifetime = 150;
+  red.scale = 0.1;
+
 }
 
+function blueBalloon() {
+  var blue = createSprite(0,Math.round(random(20, 370)), 10, 10);
+  blue.addImage(blue_balloonImage);
+  blue.velocityX = 3;
+  blue.lifetime = 150;
+  blue.scale = 0.1;
+  //write code for spwaning blue balloons
+}
 
-//creating orange leaf
-function createorangeleaf()
-{
+function greenBalloon() {
+  //write code for spwaning green balloons
+var green= createSprite(0,Math.round(random(20, 370)), 10, 10);
+green.addImage(green_balloonImage);
+green.velocityX = 3;
+green.lifetime = 150;
+green.scale = 0.1;
+}
+function pinkBalloon() {
+  var pink = createSprite(0,Math.round(random(20, 370)), 10, 10);
+  pink.addImage(pink_balloonImage);
+  pink.velocityX = 3;
+  pink.lifetime = 150;
+  pink.scale = 1.5;
 
-  if(frameCount%70===0)
- {
-   // write your code here 
-   orangeleaf = createSprite(320,20,0,30)
-   orangeleaf.scale =0.07;
-   orangeleaf.addImage(orangeleafImg);
-   orangeleaf.velocityY=2;
-   orangeleaf.x= Math.round(random(50,450))
- }
-}   
-
-rabbit.depth = orangeleaf.depth+1
-rabbit.depth = apple.depth+1;
+}
